fe592122585a906d008fc8ae2d5ea0193423b622
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2024-05-06 16:39:18 -0300

    Implementação de migrations de criaçao e população da API

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+                                                                                (1, 'Salvador, Bahia', 'Iasmim', 'Female', 'Queiroz Oliveira'),
+                                                                                (2, 'Salvador, Bahia', 'Alexandre', 'Male', 'Bacellar'),
+                                                                                (3, 'Salvador, Bahia', 'Robert', 'Cat', 'Downey Jr');
