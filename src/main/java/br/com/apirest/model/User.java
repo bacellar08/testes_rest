08e3da1533906fd447c8fa0c925cdb81e19eb168
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2024-05-28 16:39:06 -0300

    feat: começo da implementação de autenticação de login com spring secutry e jwt. finalizado o mapeamento das entidades e da tabela de relacionamento manytomany

---
+++ src/main/java/br/com/apirest/model/User.java
@@ -0,0 +1,87 @@
+package br.com.apirest.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "users")
+public class User implements UserDetails, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "full_name", nullable = false, length = 100)
+    private String fullname;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(name = "account_non_expired", nullable = false)
+    private Boolean accountNonExpired;
+
+    @Column(name = "account_non_locked", nullable = false)
+    private Boolean accountNonLocked;
+
+    @Column(name = "credentials_non_expired", nullable = false)
+    private Boolean credentialsNonExpired;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission", joinColumns = {@JoinColumn(name = "id_user")},
+    inverseJoinColumns = {@JoinColumn(name = "id_permission")})
+    private List<Permission> permissions;
+
+    public List<String> getRoles(){
+        List<String> roles = new ArrayList<>();
+        for (Permission permission : permissions ) {
+            roles.add(permission.getDescription());
+        }
+        return roles;
+    }
+
+    public User(){}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissions;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
